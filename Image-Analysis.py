25dbea45912c050549629c6fa11d04ed7b4710d2
gnaneswaran-19 <gnanesgnanes55@gmail.com>
2026-02-16 10:51:10 +0530

    Image Analysis Using Gemini with Audio

--- Image-Analysis.py
+++ Image-Analysis.py
@@ -1,12 +1,16 @@
 import google.generativeai as genai
 import streamlit as st
 from PIL import Image
-genai.configure(api_key='AIzaSyDbtoZaFMpnRys7mUHOKfZIo1GLE14Qdw8')
-model=genai.GenerativeModel('gemini-2.5-flash')     
-st.title("Image Analysis with Gemini-2.5")
-img_file=st.file_uploader("Upload an image",type=["png","jpg","jpeg"])
-prompt=st.text_input('Enter the question')
+from gtts import gTTS
+genai.configure(api_key='AIzaSyBzLQbPMe-uOHBw-wlYDg43lwCKXDkPrP4')
+model = genai.GenerativeModel('gemini-2.5-flash')
+st.title("Image Analysis")
+img_file=st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])   
+prompt=st.text_input("Enter your prompt related to the image:")
 if st.button('submit') and prompt:
     img=Image.open(img_file)
-    res=model.generate_content([prompt,img])
-    st.write(res.text)
+    response=model.generate_content([prompt,img])
+    st.write(response.text)
+    tts=gTTS(text=response.text)
+    a=tts.save("tts1.mp3")
+    st.audio("a",format="mp3")
