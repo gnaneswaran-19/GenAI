43263d988235acdd0ae6b8fcf7d97d36e430034b
gnaneswaran-19 <gnanesgnanes55@gmail.com>
2026-02-16 12:25:35 +0530

    Image Generation using pollinaiton

--- Image-Generation.py
+++ Image-Generation.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import requests
+from PIL import Image   
+st.title("Image Generation")
+prompt=st.text_input('enter your prompt')
+url="https://image.pollinations.ai/prompt/"+requests.utils.quote(prompt)
+response=requests.get(url).content
 
+if st.button('submit') and prompt:
+    st.image(response)
