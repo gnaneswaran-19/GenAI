d9c7e485807ad86e2695cef7c8c39306eaa91d9e
Gnaneswaran B S <gnanesgnanes55@gmail.com>
2026-05-07 15:55:07 +0530

    VoiceGenerator
    Replaced hardcoded API key with a placeholder.

--- VoiceGenerator.py
+++ VoiceGenerator.py
@@ -2,7 +2,7 @@ from elevenlabs.client import ElevenLabs
 import streamlit as st
 from elevenlabs import save
 st.title("Voice Generator")
-model=ElevenLabs(api_key="d25bc7865471ca5c307b59716c4b7eb26ed08b096203a10e0d02c77e10d22c25")
+model=ElevenLabs(api_key="Your API key")
 prompt=st.text_input("Enter your text here:")
 id="yrFqUM5ku2rYJCdiBKFU"
 if st.button("Generate Voice"):
